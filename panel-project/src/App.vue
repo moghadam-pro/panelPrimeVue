b4caaa6fc1eb197e59f5d455b06afc027bce1ed3
Sayid Moghadam <i@moghadam.pro>
2024-02-15 01:00:39 +0300

    first commit with sample edit and start of vue js and primevue

---
+++ panel-project/src/App.vue
@@ -0,0 +1,30 @@
+<script setup>
+import {ref} from 'vue';
+import {useToast} from "primevue/usetoast";
+
+const text = ref()
+const toast = useToast();
+const greet = () => {
+  toast.add({severity:'success', summary: 'PrimeVue JS', detail: text.value})
+}
+</script>
+
+<template>
+  <Toast></Toast>
+  <div class="container">
+    <span class="p-float-lable">
+      <InputText id="txt" type="text" v-model="text"/>
+      <label for="txt">Text</label>
+    </span>
+    <Button label="Greet" @click="greet" icon="pi pi-user"></Button>
+  </div>
+</template>
+
+<style scoped>
+.container{
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 100vh;
+}
+</style>
