493909715e6349eea4b34756bbde1638ba65483f
Sayid Moghadam <i@moghadam.pro>
2024-02-15 17:36:48 +0300

    20240215-1736

--- panel-project/src/App.vue
+++ panel-project/src/App.vue
@@ -17,8 +17,16 @@ const greet = () => {
       <label for="txt">Text</label>
     </span>
     <Button label="Greet" @click="greet" icon="pi pi-user"></Button>
+    <div class="card flex justify-content-center">
+    <Sidebar v-model:visible="visible" header="Sidebar">
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+    </Sidebar>
+    <Button icon="pi pi-arrow-right" @click="{visible = true}"></Button>
+</div>
   </div>
 </template>
+
+
 
 <style scoped>
 .container{
--- panel-project/src/main.js
+++ panel-project/src/main.js
@@ -5,19 +5,23 @@ import InputText from 'primevue/inputtext';
 import Button from 'primevue/button';
 import Toast from 'primevue/toast';
 import ToastService from 'primevue/toastservice';
+import Sidebar from 'primevue/sidebar';
+
 
 import './assets/app.css';
 import 'primevue/resources/themes/aura-light-green/theme.css';
 import 'primevue/resources/primevue.min.css';
-//import 'primeicons/primeicons.css';
+import 'primeicons/primeicons.css';
 
 const app = createApp(App);
 app.use(PrimeVue);
 app.use(ToastService);
+app.use(Sidebar);
 
 app.component('Button',Button);
 app.component('InputText',InputText);
 app.component('Toast',Toast);
+app.component('Sidebar',Sidebar);
 app.mount('#app')
 
 
